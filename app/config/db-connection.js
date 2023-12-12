0226d6832ca26502ae1c1f7cff016a7eef8bb4b3
antoniogisondi <anto.gisondi@tiscali.it>
2023-12-12 10:29:11 +0100

    modify template

--- app/config/db-connection.js
+++ app/config/db-connection.js
@@ -4,7 +4,7 @@ const { EventEmitter } = require('events');
 class DbConnection extends EventEmitter {
 
     mongoClient = new MongoClient(
-        'mongodb+srv://gianluca:gianluca123@maestro-node.3elmg.mongodb.net/passport?retryWrites=true&w=majority',
+        'mongodb://localhost:27017',
         { useNewUrlParser: true, useUnifiedTopology: true }
     );
 
@@ -12,15 +12,15 @@ class DbConnection extends EventEmitter {
         this.mongoClient.connect((err, mongodb) => {
             if (err) throw err;
             this.emit('dbConnection', {
-                db: this.mongoClient.db('passport')
+                db: this.mongoClient.db(/* mettere nome database */)
             });
             DbConnection.setInstance(mongodb);
         })
     }
 
     static setInstance(mongodb) {
-        DbConnection.db = mongodb.db('passport');
-        DbConnection.userCollection = DbConnection.db.collection('user');
+        DbConnection.db = mongodb.db(/* mettere nome database */);
+        DbConnection.userCollection = DbConnection.db.collection(/* mettere nome collection */);
     }
 }
 
